e110d87a82d19efa789b80b454f4a57f59ab5358
roberto-fernandino <romfernandino@gmail.com>
2024-08-13 20:16:32 -0300

    - Just testing the matrixes functionalityes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 mod math;
-
-use crate::math::matrix;
+use crate::math::matrix::Matrix;
 
 fn main() {
-    println!("Hello, world!");
-    // Agora você pode usar as funções ou estruturas do módulo matrix aqui
+    let mut matrix = Matrix::random(3, 3);
+    let matrix_a: Matrix = Matrix {
+        rows: 3,
+        cols: 3,
+        data: vec![
+            vec![1.0, 2.0, 3.0],
+            vec![4.0, 5.0, 6.0],
+            vec![7.0, 8.0, 9.0],
+        ],
+    };
+    println!("matrix: {:?}", matrix);
+    println!("matrix_a: {:?}", matrix_a);
+    matrix = matrix_a.subtract(&matrix);
+    println!("matrix_a_after_subtract: {:?}", matrix);
 }
