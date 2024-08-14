05153f9b8dae3f037447e3c9a8a4dfe116334f53
roberto-fernandino <romfernandino@gmail.com>
2024-08-14 16:11:26 -0300

    - Testing neural network

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
-mod math;
-use crate::math::matrix::Matrix;
+mod lib;
+use lib::{activation::SIGMOID, network::Network};
+use std::vec;
 
 fn main() {
-    math::matrix_debug::run_debug_operations();
+    let inputs = vec![
+        vec![0.0, 0.0],
+        vec![0.0, 1.0],
+        vec![1.0, 0.0],
+        vec![1.0, 1.0],
+    ];
+    let targets = vec![vec![0.0], vec![1.0], vec![1.0], vec![0.0]];
+
+    let mut network = Network::new(vec![2, 3, 1], SIGMOID, 0.5);
+
+    println!("{:?}", network.feed_forward(vec![0.0, 0.0]));
+    println!("{:?}", network.feed_forward(vec![0.0, 1.0]));
+    println!("{:?}", network.feed_forward(vec![1.0, 0.0]));
+    println!("{:?}", network.feed_forward(vec![1.0, 1.0]));
+
+    network.train(inputs, targets, 10000);
+
+    println!("{:?}", network.feed_forward(vec![0.0, 0.0]));
+    println!("{:?}", network.feed_forward(vec![0.0, 1.0]));
+    println!("{:?}", network.feed_forward(vec![1.0, 0.0]));
+    println!("{:?}", network.feed_forward(vec![1.0, 1.0]));
 }
