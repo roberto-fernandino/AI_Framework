d6d50244d39f2dc6f6a9e30ffa3e7f3067743bac
roberto-fernandino <romfernandino@gmail.com>
2024-08-20 17:44:38 -0300

    - Addded new Function INPUT to represent a function of input layer.

--- src/utils/activation.rs
+++ src/utils/activation.rs
@@ -22,8 +22,15 @@ pub const RELU: Activation = Activation {
     function: &|x| x.max(0.0),
     derivative: &|x| if x > 0.0 { 1.0 } else { 0.0 },
 };
+
 pub const LEAKY_RELU: Activation = Activation {
     name: "Leaky ReLU",
     function: &|x| x.max(0.01 * x),
     derivative: &|x| if x > 0.0 { 1.0 } else { 0.01 },
+};
+
+pub const INPUT: Activation = Activation {
+    name: "Input",
+    function: &|x| x,
+    derivative: &|x| x,
 };
