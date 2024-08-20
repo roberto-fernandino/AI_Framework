6c21be0a434c2080cf92a385a41572e029f8d85c
roberto-fernandino <romfernandino@gmail.com>
2024-08-20 17:28:33 -0300

    -Added entrypoint to main function with a cli mainloop.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,5 @@
 mod utils;
-use utils::activation::{RELU, SIGMOID};
-use utils::network::{Layers, Network};
-
+use utils::cli::mainloop;
 fn main() {
-    let inputs = vec![
-        vec![0.0, 0.0],
-        vec![0.0, 1.0],
-        vec![1.0, 0.0],
-        vec![1.0, 1.0],
-    ];
-    let targets = vec![vec![0.0], vec![1.0], vec![1.0], vec![0.0]];
-    let layers = Layers::new(vec![2, 16, 16, 1], vec![RELU, RELU, RELU, SIGMOID]);
-    let mut network = Network::new(layers, 0.01);
-    network.representation();
-
-    println!("{:?}", network.feed_forward(inputs[0].clone()));
-    println!("{:?}", network.feed_forward(inputs[1].clone()));
-    println!("{:?}", network.feed_forward(inputs[2].clone()));
-    println!("{:?}", network.feed_forward(inputs[3].clone()));
-
-    network.train(inputs.clone(), targets.clone(), 50000);
-
-    println!("{:?}", network.feed_forward(inputs[0].clone()));
-    println!("{:?}", network.feed_forward(inputs[1].clone()));
-    println!("{:?}", network.feed_forward(inputs[2].clone()));
-    println!("{:?}", network.feed_forward(inputs[3].clone()));
+    mainloop();
 }
